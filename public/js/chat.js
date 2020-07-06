72674de23cf9c4b33f0da48eabbc350ee4046183
Rohith <rohithbodagala.rit@gmail.com>
2020-07-06 23:45:17 +0530

    Increased join form size

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,9 @@
 const socket=io()
+// var siteWidth = 1280;
+// var scale = screen.width /siteWidth
+
+// document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
+//<meta name="viewport" content="width=1280, initial-scale=1">
 
 //Elements
 const $messageForm=document.getElementById('message-form')
